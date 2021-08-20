3248e63db33d7a3e12e1892520fb5a5dd133a1b0
Sandeep Sakilam <Sandeep.Sakilam@jetblue.com>
2021-08-20 19:59:39 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jetb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'jetboard-ui';
+}
